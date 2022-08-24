db0da664fd0739e66d73d8be523b50f3744977e1
Y_Less <git-athena@y-less.com>
2022-08-24 08:04:21 +0100

    Start the open.mp upgrade code project.

---
+++ CallbackUpgrade/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace CallbackUpgrade
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			if (args.Length == 1 || args.Contains("--help"))
+			{
+				// Display the help message.
+				Console.WriteLine("Upgrades forward/hook/public prototypes from SA:MP to open.mp");
+				Console.WriteLine("");
+				Console.WriteLine("Usage:");
+				Console.WriteLine("");
+				Console.WriteLine("    callback_upgrade [--report] [--help] directory");
+				Console.WriteLine("");
+				Console.WriteLine("  --report - Show changes to make, but don't make them.");
+				Console.WriteLine("  --help - Show this message and exit.");
+				Console.WriteLine("  directory - Where to run the scan.");
+			}
+			else
+			{
+
+			}
+		}
+	}
+}
+
---
+++ CallbackUpgrade/Scanner.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CallbackUpgrade
+{
+	class Scanner
+	{
+		public bool Report { private set; get; }
+
+		public Scanner(bool report)
+		{
+			Report = report;
+		}
+	}
+}
+
