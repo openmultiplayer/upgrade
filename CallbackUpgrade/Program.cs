6f8e972e953d779dd56618ae2883038696762237
Y_Less <git-athena@y-less.com>
2022-08-25 15:46:53 +0100

    Load the JSON.

--- CallbackUpgrade/Program.cs
+++ CallbackUpgrade/Program.cs
@@ -2,13 +2,34 @@
 using System.Linq;
 using PCRE;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.IO;
+using System.Collections.Generic;
 
 namespace CallbackUpgrade
 {
+	public class Replacement
+	{
+		public string description { get; set; }
+		public string from { get; set; }
+		public string to { get; set; }
+	}
+
+	public class Scans
+	{
+		public Dictionary<string, string> defines { get; set; }
+		public Replacement[] replacements { get; set; }
+	}
+
 	class Program
 	{
 		static void Main(string[] args)
 		{
+			using (StreamReader file = File.OpenText(@"../../../callback-upgrade.json"))
+			{
+				JsonSerializer serializer = new JsonSerializer();
+				Scans scans = (Scans)serializer.Deserialize(file, typeof (Scans));
+			}
 			if (args.Length == 0 || args.Contains("--help"))
 			{
 				// Display the help message.
