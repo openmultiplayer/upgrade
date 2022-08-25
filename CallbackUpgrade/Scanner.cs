a63783350867620de6ced03c4cf5b04c5c117e5f
Y_Less <git-athena@y-less.com>
2022-08-25 20:19:41 +0100

    Doing the replacements once.

--- CallbackUpgrade/Scanner.cs
+++ CallbackUpgrade/Scanner.cs
@@ -54,16 +54,7 @@ namespace CallbackUpgrade
 					// reading of the documentation).  Each replacement is done separately.
 					var regex = new PcreRegex(RegexDefine + rep.From, PcreOptions.Compiled | PcreOptions.Extended | PcreOptions.MultiLine);
 					using var buffer = regex.CreateMatchBuffer();
-					foreach (var match in buffer.Matches(contents))
-					{
-						var diff = new Diff {
-							Description = rep.Description,
-							From = match.Value.ToString(),
-							To = rep.To,
-							Line = match.Index
-						};
-						ret.Add(diff);
-					}
+					contents = regex.Replace(contents, rep.To);
 				}
 			}
 			// Returns a list of the replacements to be made.
