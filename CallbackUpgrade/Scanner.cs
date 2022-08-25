380a8f98ce3004b432dca5b9cc8909ea11a47a9f
Y_Less <git-athena@y-less.com>
2022-08-25 19:15:13 +0100

    Create `DEFINE` section.

--- CallbackUpgrade/Scanner.cs
+++ CallbackUpgrade/Scanner.cs
@@ -1,15 +1,44 @@
 ﻿using Newtonsoft.Json;
 using System.Collections.Generic;
+using System.Text;
 
 namespace CallbackUpgrade
 {
 	class Scanner
 	{
+		private Dictionary<string, string> defines_ = null;
+
 		[JsonProperty("defines")]
-		public Dictionary<string, string> Defines { get; set; }
+		public Dictionary<string, string> Defines
+		{
+			get
+			{
+				return defines_;
+			}
+			
+			set
+			{
+				defines_ = value;
+				// Construct the regex version of these defines.
+				StringBuilder sb = new StringBuilder("(?(DEFINE)");
+				foreach (var kv in value)
+				{
+					sb.Append("\n  (?<");
+					sb.Append(kv.Key);
+					sb.Append(">\n    ");
+					sb.Append(kv.Value);
+					sb.Append("\n  )");
+				}
+				sb.Append("\n)\n");
+				RegexDefine = sb.ToString();
+			}
+		}
 
 		[JsonProperty("replacements")]
 		public Replacement[] Replacements { get; set; }
+
+		[JsonIgnore]
+		public string RegexDefine { get; private set; }
 
 		public Diff[] Report(string name)
 		{
