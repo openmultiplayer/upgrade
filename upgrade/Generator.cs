8286b864593396060388519302ca7986df9ab0b9
Y_Less <git-athena@y-less.com>
2022-08-27 15:57:21 +0100

    Add a license.

--- upgrade/Generator.cs
+++ upgrade/Generator.cs
@@ -1,6 +1,13 @@
-﻿using Newtonsoft.Json;
+﻿/*
+ *  This Source Code Form is subject to the terms of the Mozilla Public License,
+ *  v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ *  obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ *  The original code is copyright (c) 2022, open.mp team and contributors.
+ */
+
+using Newtonsoft.Json;
 using PCRE;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
